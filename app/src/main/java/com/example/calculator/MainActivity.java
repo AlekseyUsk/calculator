d8dc563b9a1e3034f97d4055a240c027981aebc5
Aleksey <meysonsib200189@mail.ru>
2022-04-03 02:25:40 +0700

    calculaterTask4 домашняя работа на проверку(FATAL EXCEPTION)Делаю выбор темы

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -14,68 +14,66 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.Serializable;
-import java.util.List;
+public class MainActivity extends AppCompatActivity {
+    //  private static final String THEME_KEY = "THEME_KEY";
 
-public class MainActivity extends AppCompatActivity implements Serializable {
+    // private static final String THEME_TWO = "THEME_TWO";
 
     public static final String TAG = "myLog";
-    protected static String oldNumber = "oldNumber"; // ключ значение для onSaveInstanceState
-    protected String operator;            // арифметические действия при нажатии кнопок(пока незадействовал)
-    protected String number = "number";   // числа при нажатии кнопок
 
-    protected TextView textView;
+    private static final String OLDNUMBER = "oldNumber"; // ключ значение для onSaveInstanceState
+    String oldNumber;
+    String operator;            // арифметические действия при нажатии кнопок(пока незадействовал)
+    String number;   // числа при нажатии кнопок
 
-    protected Button zero;
-    protected Button one;
-    protected Button two;
-    protected Button three;
-    protected Button four;
-    protected Button five;
-    protected Button six;
-    protected Button seven;
-    protected Button eight;
-    protected Button nine;
-    protected Button sum;
-    protected Button division;
-    protected Button multiply;
-    protected Button result;
-    protected Button cha;
-    protected Button min;
-    protected Button del;
-    boolean isNew = true;// переменная чтобы убрать ввод 0 при начальном вводе
+    TextView textView;
 
+    Button zero;
+    Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
+    Button nine;
+    Button sum;
+    Button division;
+    Button multiply;
+    Button result;
+    Button cha;
+    Button min;
+    Button del;
+    boolean isNew = true;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);//внутри метод getMenuInflater() который создает или раздувает наше меню из ресурсов
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.item1:
-                Toast.makeText(getApplicationContext(), "Выбрана тема 1", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "НАЖАТИЕ НА item1");
-                break;
-            case R.id.item2:
-                Toast.makeText(getApplicationContext(), "Выбрана тема 2", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "НАЖАТИЕ НА item2");
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //   SharedPreferences sharedPreferences = getSharedPreferences("SharedPreferences", Context.MODE_PRIVATE);
+        //  setTheme(R.style.Theme_Calculatorv2);
+
+
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            oldNumber = (String) savedInstanceState.get("oldNumber");
+            showResult();
+        }
 
-        if (savedInstanceState != null)
-            //  number = (String) savedInstanceState.getSerializable(number);как то попробовать записать в Bundle другие переменные?
-            oldNumber = (String) savedInstanceState.getSerializable(oldNumber);
+
+      /*  findViewById(R.id.item2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sharedPreferences.edit();
+
+                recreate();
+            }
+        });*/
 
 
         TextView textView = findViewById(R.id.textView);
@@ -104,12 +102,14 @@ public class MainActivity extends AppCompatActivity implements Serializable {
             @Override
             public void onClick(View view) {
 
-
-                if (isNew)
-                    textView.setText(""); //убрал ноль вначале ввода
+                if (isNew) {
+                    textView.setText("");
+                }
                 isNew = false;
 
-                number = textView.getText().toString();
+                String number = textView.getText().toString();
+                String oldNumber = textView.getText().toString();
+
                 switch (view.getId()) {
                     case R.id.zero:
                         number = number + "0";
@@ -160,11 +160,20 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                         Log.d(TAG, "НАЖАТА КНОПКА +/-");
                         break;
                 }
-                oldNumber = number; // В oldNumber записываю нажатые цифры
+                oldNumber = number;
                 textView.setText(oldNumber); // на экран вывожу при вводе цифры
             }
-
         };
+        zero.setOnClickListener(numberButtonClickListener);
+        one.setOnClickListener(numberButtonClickListener);
+        two.setOnClickListener(numberButtonClickListener);
+        three.setOnClickListener(numberButtonClickListener);
+        four.setOnClickListener(numberButtonClickListener);
+        five.setOnClickListener(numberButtonClickListener);
+        six.setOnClickListener(numberButtonClickListener);
+        seven.setOnClickListener(numberButtonClickListener);
+        eight.setOnClickListener(numberButtonClickListener);
+        nine.setOnClickListener(numberButtonClickListener);
 
         View.OnClickListener actionClickListener = new View.OnClickListener() {
             @Override
@@ -203,17 +212,8 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                 }
                 textView.setText(operator);
             }
+
         };
-        zero.setOnClickListener(numberButtonClickListener);
-        one.setOnClickListener(numberButtonClickListener);
-        two.setOnClickListener(numberButtonClickListener);
-        three.setOnClickListener(numberButtonClickListener);
-        four.setOnClickListener(numberButtonClickListener);
-        five.setOnClickListener(numberButtonClickListener);
-        six.setOnClickListener(numberButtonClickListener);
-        seven.setOnClickListener(numberButtonClickListener);
-        eight.setOnClickListener(numberButtonClickListener);
-        nine.setOnClickListener(numberButtonClickListener);
         sum.setOnClickListener(actionClickListener);
         division.setOnClickListener(actionClickListener);
         multiply.setOnClickListener(actionClickListener);
@@ -224,64 +224,49 @@ public class MainActivity extends AppCompatActivity implements Serializable {
         minplus.setOnClickListener(actionClickListener);
     }
 
-
-    /**
-     * -------СОХРАНЕНИЕ СОСТОЯНИЯ------
-     */
- /*   @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putSerializable(oldNumber, oldNumber);
-        outState.putSerializable(number, number);
-        Log.d(TAG, "onSaveInstanceState: СОХРАНЕНИЕ ДАННЫХ");
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);//внутри метод getMenuInflater() который создает или раздувает наше меню из ресурсов
+        return true;
     }
 
     @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        oldNumber = (String) savedInstanceState.getSerializable(oldNumber);
-        Log.d(TAG, "onRestoreInstanceState: ВОССТАНОВЛЕНИЕ ДАННЫХ");
-        setTextCalc(oldNumber);
-
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "Выбрана тема 1", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "НАЖАТИЕ НА item1");
+                break;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(), "Выбрана тема 2", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "НАЖАТИЕ НА item2");
+                //  setTheme(R.style.Theme_Calculatorv2);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    public void setTextCalc(String oldNumber) {
-        Log.d(TAG, "МЕТОД ВЫЗОВА ИНФОРМАЦИИ НА ЭКРАН");
-        textView.setText(this.oldNumber);
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("oldNumber", oldNumber);
+        Log.d(TAG, "onSaveInstanceState: СОХРАНЕНИЕ ДАННЫХ");
+    }
+
+
+
+   /* @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        oldNumber = (String) savedInstanceState.getSerializable("oldNumber");
+        Log.d(TAG, "onRestoreInstanceState: ВОССТАНОВЛЕНИЕ ДАННЫХ");
+        showResult();
+
     }*/
+
+    private void showResult() {
+        Log.d(TAG, "МЕТОД ВЫЗОВА ИНФОРМАЦИИ НА ЭКРАН");
+        textView.setText(oldNumber);
+    }
+
 }
-/**
- * ---------------При повороте экрана тупо вылетает приложение(вот в логах что отображает)немогу понять что вызвало мою FATAL EXCEPTION: main
- * ----------------и по сохранению состояния я все правильно сделал?
- * 2022-03-28 14:14:03.439 21000-21000/com.example.calculator D/myLog: onSaveInstanceState: СОХРАНЕНИЕ ДАННЫХ
- * 2022-03-28 14:14:03.441 21000-21000/com.example.calculator D/ActivityThread: Remove activity client record, r= ActivityRecord{9d71e8b token=android.os.BinderProxy@8b8c1fb {com.example.calculator/com.example.calculator.MainActivity}} token= android.os.BinderProxy@8b8c1fb
- * 2022-03-28 14:14:03.477 21000-21050/com.example.calculator W/libEGL: EGLNativeWindowType 0x6f4ae456d0 disconnect failed
- * 2022-03-28 14:14:03.525 21000-21000/com.example.calculator V/ActivityThread: callActivityOnCreate
- * 2022-03-28 14:14:03.543 21000-21000/com.example.calculator I/DecorView[]:  old windowMode:0 new windoMode:1
- * 2022-03-28 14:14:03.754 21000-21000/com.example.calculator D/ActivityThread: add activity client record, r= ActivityRecord{9d71e8b token=android.os.BinderProxy@8b8c1fb {com.example.calculator/com.example.calculator.MainActivity}} token= android.os.BinderProxy@8b8c1fb
- * 2022-03-28 14:14:03.760 21000-21000/com.example.calculator D/myLog: onRestoreInstanceState: ВОССТАНОВЛЕНИЕ ДАННЫХ
- * 2022-03-28 14:14:03.760 21000-21000/com.example.calculator D/myLog: МЕТОД ВЫЗОВА ИНФОРМАЦИИ НА ЭКРАН
- * 2022-03-28 14:14:03.761 21000-21000/com.example.calculator D/AndroidRuntime: Shutting down VM
- * 2022-03-28 14:14:03.761 21000-21000/com.example.calculator I/QarthLog: [PatchStore] createDisableExceptionQarthFile
- * 2022-03-28 14:14:03.761 21000-21000/com.example.calculator I/QarthLog: [PatchStore] create disable file for com.example.calculator uid is 10118
- * 2022-03-28 14:14:03.763 21000-21000/com.example.calculator E/AndroidRuntime: FATAL EXCEPTION: main
- * Process: com.example.calculator, PID: 21000
- * java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.TextView.setText(java.lang.CharSequence)' on a null object reference
- * at com.example.calculator.MainActivity.setTextCalc(MainActivity.java:222)
- * at com.example.calculator.MainActivity.onRestoreInstanceState(MainActivity.java:216)
- * at android.app.Activity.performRestoreInstanceState(Activity.java:1580)
- * at android.app.Instrumentation.callActivityOnRestoreInstanceState(Instrumentation.java:1367)
- * at android.app.ActivityThread.handleStartActivity(ActivityThread.java:3937)
- * at android.app.servertransaction.TransactionExecutor.performLifecycleSequence(TransactionExecutor.java:235)
- * at android.app.servertransaction.TransactionExecutor.cycleToPath(TransactionExecutor.java:215)
- * at android.app.servertransaction.TransactionExecutor.executeLifecycleState(TransactionExecutor.java:187)
- * at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:105)
- * at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2473)
- * at android.os.Handler.dispatchMessage(Handler.java:110)
- * at android.os.Looper.loop(Looper.java:219)
- * at android.app.ActivityThread.main(ActivityThread.java:8349)
- * at java.lang.reflect.Method.invoke(Native Method)
- * at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:513)
- * at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:1055)
- * 2022-03-28 14:14:03.785 21000-21000/com.example.calculator I/Process: Sending signal. PID: 21000 SIG:
- */
